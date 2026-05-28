ab50f68d9cc8358580d74565f79db247e38a2de7
Sanish Poudel <139324676+SanishPoudel@users.noreply.github.com>
2026-05-29 09:03:35 +1000

    added methods inside room

--- models/room.py
+++ models/room.py
@@ -6,3 +6,17 @@ class Room:
         self.nightly_rate = nightly_rate
         self.status = status
         self.capacity = capacity
+
+    def display_info(self):
+        print(
+            f"Room {self.room_number} |"
+            f"{self.room_type} |"
+            f"{self.nightly_rate} |"
+            f"{self.status}"
+        )
+
+    def set_status(self, status):
+        self.status = status
+
+    def is_available(self):
+        return self.status.lower() == "active"
